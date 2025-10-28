fdbc97488e9f0a9464f9bf2374ab7fe650737063
Ruben <174733720+ruben-23@users.noreply.github.com>
2025-10-28 20:20:37 +0200

    Initial commit

---
+++ src/main/java/org/example/licentaapp/LicentaAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.licentaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicentaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicentaAppApplication.class, args);
+	}
+
+}
